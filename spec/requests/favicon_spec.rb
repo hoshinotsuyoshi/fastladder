d479ba1b9489ea85da8f635d6bc4aa0ba2ace6ad
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2020-07-25 14:24:25 +0900

    Suppress a warning

--- spec/requests/favicon_spec.rb
+++ spec/requests/favicon_spec.rb
@@ -6,7 +6,7 @@ describe 'favicon' do
 
     it 'returns favicon' do
       expect(Feed).to receive(:find_by).with(feedlink: feed.link).and_call_original
-      get "/favicon/#{feed.link}"
+      get "/favicon/#{feed.link}", params: {}, headers: {}
       expect(response).to be_ok
       expect(response.content_type).to eq('image/png')
     end
